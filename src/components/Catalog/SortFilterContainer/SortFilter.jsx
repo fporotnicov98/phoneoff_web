4e918df681f962f77bc5f9d0d782b17a69027462
golovan8672 <golovan8672@gmail.com>
2020-05-21 21:54:22 +0500

    add activeOnClick

--- src/components/Catalog/SortFilterContainer/SortFilter.jsx
+++ src/components/Catalog/SortFilterContainer/SortFilter.jsx
@@ -1,26 +1,42 @@
 import React from 'react';
 import style from './SortFilter.module.scss'
 
-const SortFilter = (props) => (
-    <div className={style['buttons']}>
-        <button className={style['btn']}
-            active={props.filterBy === 'all'}
-            onClick={props.setFilter.bind(this, 'all')}>
-            Все товары
-        </button>
-        <button className={style['btn']}
-            active={props.filterBy === 'price_high'}
-            onClick={props.setFilter.bind(this, 'price_high')}>
-            Цена (дорогие)
-        </button>
-        <button className={style['btn']}
-            active={props.filterBy === 'price_low'}
-            onClick={props.setFilter.bind(this, 'price_low')}>
-            Цена (дешевые)
-        </button>
-    </div>
+class SortFilter extends React.Component {
+    state = {
+        isActive: 'all'
+    }
 
+    render() {
+        return (
+            <div className={style['buttons']}>
+                <button className={this.state.isActive === "all" ? style['active_btn'] : style['btn']}
+                    active={this.props.filterBy === 'all'}
+                    onClick={()=> {
+                        this.props.setFilter('all')
+                        this.setState({ isActive: 'all' })
+                    }}>
+                    Все товары
+            </button>
+                <button className={this.state.isActive === "price_high" ? style['active_btn'] : style['btn']}
+                    active={this.props.filterBy === 'price_high'}
+                    onClick={() => {
+                        this.props.setFilter('price_high')
+                        this.setState({ isActive: 'price_high'})
 
-);
+                        }}>
+                    Цена (дорогие)
+            </button>
+                <button className={this.state.isActive === "price_low" ? style['active_btn'] : style['btn']}
+                    active={this.props.filterBy === 'price_low'}
+                    onClick={() => {
+                        this.props.setFilter('price_low')
+                        this.setState({ isActive: 'price_low' })
+                    }}>
+                    Цена (дешевые)
+            </button>
+            </div>
+        )
+    }
+}
 
 export default SortFilter;
